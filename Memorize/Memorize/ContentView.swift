8bc4face67197e221597ee5181432ef6bfc441a5
Kirill <Kirill07478@gmail.com>
2021-10-11 20:40:24 +0300

    First Lection

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Kiryusha Orlov on 09.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            ForEach(0..<4) { index in
+                CardView()
+            }
+        }
+            .foregroundColor(Color.orange)
+            .padding()
+            .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0)
+                    .fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0)
+                    .stroke(lineWidth: 3.0)
+                Text("👻")
+            } else  {
+                RoundedRectangle(cornerRadius: 10.0)
+                    .fill(Color.orange)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
